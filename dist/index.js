8c2741e682809497c8c0e126baf2e3128f3b7efa
Michael Shilman <michael@lab80.co>
2021-11-17 00:52:17 +0800

    Update package lock

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,26 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 "use strict";
 
+/* eslint-disable @typescript-eslint/no-explicit-any  */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,17 +35,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.githubClient = void 0;
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const node_fetch_1 = __importDefault(__nccwpck_require__(894));
 const GITHUB_API = 'https://api.github.com/graphql';
+// Jest shenanigans
+const fetch = (url, options) => 
+// eslint-disable-next-line github/no-then
+Promise.resolve().then(() => __importStar(__nccwpck_require__(894))).then(({ default: actualFetch }) => __awaiter(void 0, void 0, void 0, function* () { return actualFetch(url, options); }));
 const githubClient = (apiKey) => {
     const client = (query, variables) => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, node_fetch_1.default)(GITHUB_API, {
+        const res = yield fetch(GITHUB_API, {
             method: 'POST',
             headers: {
                 authorization: `token ${apiKey}`
